b554bc3c552922aded6085fcf969f1e695646124
Steven Boulom <sboulom@gmail.com>
2020-01-14 11:58:34 -0800

    Indexfile

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/*
+special charcters = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+numeric characters = 0123456789
+lowercase characters = abcdefghijklmnopqrstuvwxyz
+Uppercase characters = ABCDEFGHIJKLMNOPQRSTUVWXYZ
+length = 8 to 128 characters
+
+*/
+
+document.querySelector("#passwordBtn").addEventListener("click",function(){
+    alert("clicked")
+})
