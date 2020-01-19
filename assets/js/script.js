7dbc43d567bddfa5d512695e41f8d5f439f5f680
Steven Boulom <sboulom@gmail.com>
2020-01-18 21:32:11 -0800

    password generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,31 @@
-/*
-special charcters = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-numeric characters = 0123456789
-lowercase characters = abcdefghijklmnopqrstuvwxyz
-Uppercase characters = ABCDEFGHIJKLMNOPQRSTUVWXYZ
-length = 8 to 128 characters
+function generate(){
 
-*/
+    let complexity = document.getElementById("slider").value;
+    
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789";
+    
+    let password = "";
+    
 
-document.querySelector("#passwordBtn").addEventListener("click",function(){
-    alert("clicked")
-})
+    for(var i =0; i <= complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+    }
+    
+    document.getElementById("display").value = password;
+
+}
+
+document.getElementById("slider").oninput = function(){
+
+    if(document.getElementById("slider").value >0) {
+        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;    }
+}
+
+function copyToClipboard(){
+
+    document.getElementById("display").select();
+
+    document.execCommand("Copy");
+
+    alert("Copied to Clipboard");
+}
