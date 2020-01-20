e4370418c8cb95fbceda4225a9d47bb1e9418fc5
Steven Boulom <sboulom@gmail.com>
2020-01-19 22:52:27 -0800

    Updated JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,83 @@
+let specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("")
+let numbers = "0123456789".split("")
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+let lowerCase = "abcdefghijklmnopqrstuvwxyz".split("")
+let allPassword = []
+let size = 0
+
+let tempAllPassword =[]
 function generate(){
 
-    let complexity = document.getElementById("slider").value;
+
+    let passwordLength = prompt("What is the size of your password (8-128)?")
+        let isSpecialCharacters = confirm ("do you want special characters in your password?")
+        let isNumber = confirm ("do you want numbers in your password?")
+        let isUpperCase = confirm ("do you want upper case characters in your password?")
+        let isLowerCase = confirm ("do you want lower case characters in your password?")
+
+        if(passwordLength >= 8 && passwordLength <= 128) {
+            size = passwordLength
+            makePassword(isSpecialCharacters, isNumber, isUpperCase, isLowerCase, passwordLength)
+        }
+        else{
+            alert("Invalid password length (8-128)")
+        }
+
+    // let complexity = document.getElementById("slider").value;
     
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789";
+    // let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789";
     
-    let password = "";
+    // let password = "";
     
 
-    for(var i =0; i <= complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
-    }
+    // for(var i =0; i <= complexity; i++){
+    //     password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+    // }
     
-    document.getElementById("display").value = password;
+    // // document.getElementById("display").value = password;
+    // document.getElementById("Password").innerHTML=password
 
 }
+
+
+function addCharacters(array, checking, passwordLength, index){
+    shuffle(array) 
+    console.log(array)
+
+   if(checking){
+     tempAllPassword = [...array]
+    allPassword.push(array[index])
+    passwordLength = passwordLength-1
+   }
+
+}
+
+
+function makePassword(isSpecialCharacters, isNumber, isUpperCase, isLowerCase, passwordLength){
+    
+    addCharacters(specialCharacters, isSpecialCharacters, passwordLength, 0)
+    addCharacters(numbers, isNumber, passwordLength, 0)
+    addCharacters(upperCase, isUpperCase, passwordLength, 0)
+    addCharacters(lowerCase, isLowerCase, passwordLength, 0)
+
+  
+   shuffle(allPassword)
+
+   shuffle (tempAllPassword)
+   for (let index = 0; index < passwordLength; index++) {
+        
+            allPassword.push(tempAllPassword[index])
+
+   }
+   shuffle(allPassword)
+    document.getElementById("Password").innerHTML = allPassword.join("")
+}
+
+function shuffle(arr) {
+    arr.sort(function(a,b) {
+      return (0.5 - Math.random());
+    });
+  }
 
 document.getElementById("slider").oninput = function(){
 
